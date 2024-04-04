c1cfdc7c0f0643c7510baca724bad46bde1232b3
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-04-04 21:45:56 +0700

    03. Exception Handling: Try with Resources

--- src/main/java/org/iffat/file_exceptions/Main.java
+++ src/main/java/org/iffat/file_exceptions/Main.java
@@ -1,6 +1,8 @@
 package org.iffat.file_exceptions;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -25,13 +27,38 @@ public class Main {
 
 	private static void testFile(String filename) {
 		Path path = Paths.get(filename);
+		FileReader reader = null;
 		try {
-			List<String> lines = Files.readAllLines(path);
+			// List<String> lines = Files.readAllLines(path);
+			reader = new FileReader(filename);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
 			System.out.println("Maybe I'd log something either way...");
 		}
 		System.out.println("File exists and able to use as a resource");
+	}
+
+	private static void testFile2(String filename) {
+
+		try (FileReader reader = new FileReader(filename)) {
+		} catch (FileNotFoundException e) {
+			System.out.println("File '" + filename + "' does not exists");
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} catch (Exception e) {
+			System.out.println("Something unrelated and unexpected happened");
+		} finally {
+			System.out.println("Maybe I'd log something either way...");
+		}
+		System.out.println("File exists and able to use as resource");
 	}
 }
