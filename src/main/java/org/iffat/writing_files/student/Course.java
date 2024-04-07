d3ffdba9b233beff3059a00699b28a3f18d5818d
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-04-07 07:44:30 +0700

    14. Writing Data To a File

---
+++ src/main/java/org/iffat/writing_files/student/Course.java
@@ -0,0 +1,13 @@
+package org.iffat.writing_files.student;
+
+public record Course(String courseCode, String title) {
+
+	public int getLectureCount() {
+		return 15;
+	}
+
+	@Override
+	public String toString() {
+		return "%s %s".formatted(courseCode, title);
+	}
+}
